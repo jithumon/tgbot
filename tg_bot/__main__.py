52a89071625492ce37c634cc0dd59d5316477126
Paul Larsen <pn1315@ic.ac.uk>
2018-04-08 22:40:42 +0100

    Make sure to delete the previous message when going through /help or /settings to avoid chat spam

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -213,6 +213,7 @@ def help_button(bot: Bot, update: Update):
 
         # ensure no spinny white circle
         bot.answer_callback_query(query.id)
+        query.message.delete()
     except BadRequest as excp:
         if excp.message == "Message is not modified":
             pass
@@ -326,6 +327,7 @@ def settings_button(bot: Bot, update: Update):
 
         # ensure no spinny white circle
         bot.answer_callback_query(query.id)
+        query.message.delete()
     except BadRequest as excp:
         if excp.message == "Message is not modified":
             pass
