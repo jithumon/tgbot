9f9302201c41ccf4c463890cc0effb139a4a088c
jithumon <jithu311990@gmail.com>
2020-08-05 15:30:26 +0530

    Fix Gban reply

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -159,9 +159,12 @@ def gban(bot: Bot, update: Update, args: List[str]):
         except TelegramError:
             pass
 
+    gban_complete="{} has been successfully gbanned :)".format(mention_html(user_chat.id, user_chat.first_name or "Deleted Account"))
+    keyboard = []
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
                    "{} has been successfully gbanned :)".format(mention_html(user_chat.id, user_chat.first_name or "Deleted Account")),
                    html=True)
+    message.reply_text(gban_complete, reply_markup=keyboard, parse_mode=ParseMode.HTML)
 
 @run_async
 def ungban(bot: Bot, update: Update, args: List[str]):
