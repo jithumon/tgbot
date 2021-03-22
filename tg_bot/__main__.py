51bc9a6f2d3aecbbfc453f2239958d67c458301f
Shrimadhav U K <shrimadhavuk@gmail.com>
2021-03-22 13:02:54 +0530

    fix for new line in alert filters

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -428,10 +428,12 @@ def kcfrsct_fnc(bot: Bot, update: Update):
             from tg_bot.modules.sql.cust_filters_sql import get_btn_with_di
             _soqka = get_btn_with_di(int(_match.group(1)))
             query.answer(
-                text=_soqka.url,
+                text=_soqka.url.replace("\\n", "\n").replace("\\t", "\t"),
+                # HPFPOCWBANER: https://stackoverflow.com/a/42965750
                 show_alert=True
             )
-        except:
+        except Exception as e:
+            print(e)
             bot.answer_callback_query(query.id)
 
 
